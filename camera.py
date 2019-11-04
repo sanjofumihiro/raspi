ee19a63d7fe9fec327951df77feed494cdc19381
sanjofumihiro <sanjofumihiro@gmail.com>
2019-11-04 15:14:38 +0900

    [add]camera.py

---
+++ camera.py
@@ -0,0 +1,24 @@
+import cv2
+import time
+import picamera
+import threading
+
+streamPath="./c.jpg"
+streamRate=0.1
+
+def refreshPic():
+    while True:
+        with picamera.PiCamera() as camera:
+            camera.resolution=(1080,860)
+            camera.start_preview()
+            time.sleep(2)
+            while True:
+                camera.capture(streamRate)
+                time.sleep(streamRate)
+thread_pic=threading.Thread(target=refreshPic)
+thread_pic.start()
+
+def ballPosition():
+    image=cv2.imread(streamPath)
+    cv2.imshow(image)
+ballPosition()
